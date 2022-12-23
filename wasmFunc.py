1499df1723e34cb4d9b06234c17343d8a0ff215f
Ilie Dumitru <85458480+DumitruIlie@users.noreply.github.com>
2022-12-23 14:29:29 +0200

    Add coments

--- wasmFunc.py
+++ wasmFunc.py
@@ -8,7 +8,7 @@ class wasmFunc:
 		self.name=""
 		self.tokens=[]
 		
-	#aici setez parametri, returnType-urile si tokenele specifice
+	#aici setez parametri, returnType-urile si lista de tokene(instructiunile pe care le executa functia)
 	def make(self, T, poz):
 		if T[poz].token!='func':
 			return -1
@@ -17,46 +17,52 @@ class wasmFunc:
 			return -1
 		while T[poz].tokType=='start':
 			poz+=1
-			#nume
 			if T[poz].token=='export':
+				#numele functiei, folosit la apel
 				poz+=1
 				self.name=T[poz].token.strip("\"")
 				poz+=2
-			#parametrii
+			
 			elif T[poz].token=='param':
+				#parametrii(tip si alias)
 				poz+=1
 				while T[poz].tokType!='end':
 					if T[poz].tokType=='alias':
 						#parametru cu alias
-						self.params[T[poz].token]=len(self.paramTypes)-1
+						self.params[T[poz].token]=len(self.paramTypes)
 						poz+=1
 					self.paramTypes.append(T[poz].token)
 					poz+=1
 				poz+=1
-			#variabile locale
+			
 			elif T[poz].token=='local':
+				#variabile locale(tip si alias)
 				poz+=1
 				while T[poz].tokType!='end':
 					if T[poz].tokType=='alias':
-						#parametru cu alias
-						self.params[T[poz].token]=len(self.paramTypes)-1
+						#variabila locala cu alias
+						self.params[T[poz].token]=len(self.paramTypes)+len(self.localTypes)
 						poz+=1
 					self.paramTypes.append(T[poz].token)
 					poz+=1
 				poz+=1
-			#results
+			
 			elif T[poz].token=='result':
+				#tip de return
 				poz+=1
 				while T[poz].tokType!='end':
 					self.results[len(self.results)]=T[poz].token
 					poz+=1
 				poz+=1
-			#tokene
+			
 			else:
+				#instructiunile functiei, le stochez ca o lista ca pot folosi aceeasi functie de evaluare
 				cntParant=1
 				while poz<len(T):
 					if cntParant==1 and T[poz].tokType=='end':
+						#sfarsit corp functie
 						return poz+1
+					
 					if T[poz].tokType=='start':
 						cntParant+=1
 					elif T[poz].tokType=='end':
@@ -64,4 +70,5 @@ class wasmFunc:
 					self.tokens.append(T[poz])
 					poz+=1
 				return -1
+			
 		return -1
