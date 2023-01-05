d192d095e59f9c2b40a4023e4c3b920ab286acfc
SadCarrotMaru <marut.rogr@gmail.com>
2023-01-05 20:33:38 +0200

    added f32, i8 si i16

---
+++ f32.py
@@ -0,0 +1,5 @@
+class f32:
+    def __init__(self, val):
+        self._val = val
+    def __str__(self):
+        return f"{self._val}"
---
+++ i8.py
@@ -0,0 +1,5 @@
+class i8:
+    def __init__(self, val):
+        self._val = val
+    def __str__(self):
+        return f"{self._val}"
